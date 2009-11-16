618e1faa7f579640312a7eba42a9875ba56b3eab
Audun D. Christoffersen <audunc@met.no>
2009-11-16 16:52:05 +0000

    fixes

--- tsData.h
+++ tsData.h
@@ -1,38 +1,37 @@
 /*
-  libpuDatatypes - Diverse datatypes: Regions, coordinates and alike
-  
-  $Id$
+ libpuDatatypes - Diverse datatypes: Regions, coordinates and alike
 
-  Copyright (C) 2006 met.no
+ $Id$
 
-  Contact information:
-  Norwegian Meteorological Institute
-  Box 43 Blindern
-  0313 OSLO
-  NORWAY
-  email: diana@met.no
-  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ Copyright (C) 2006 met.no
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ Contact information:
+ Norwegian Meteorological Institute
+ Box 43 Blindern
+ 0313 OSLO
+ NORWAY
+ email: diana@met.no
 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #ifndef _tsData_h
 #define _tsData_h
 
 // template class to hold a timeserie of one parameter.
-// JS/PU 9/99 
+// JS/PU 9/99
 
 #include <map>
 #include <set>
@@ -41,116 +40,162 @@
 #include <puTools/miTime.h>
 #include <puTools/miString.h>
 
-
 using namespace std;
 
 namespace puData {
-  
-  template<class V> struct progVal {
-  
-    V value;
-    int prog;
-    
-    progVal():   prog(0) {}
-    progVal(V val): value(val), prog(0) {}  
-    progVal(V val, int pr):  value(val), prog(pr) {} 
-    friend progVal<V> operator+(const progVal<V>& lhs, const progVal<V>& rhs)
-    { return progVal((rhs.value+lhs.value), rhs.prog);}
-    friend progVal<V> operator-(const progVal<V>& lhs, const progVal<V>& rhs)
-    { return progVal((rhs.value-lhs.value), rhs.prog);}
-    friend progVal<V> operator*(const progVal<V>& lhs, const progVal<V>& rhs)
-    { return progVal((rhs.value*lhs.value), rhs.prog);}
-    friend progVal<V> operator/(const progVal<V>& lhs, const progVal<V>& rhs)
-    { return progVal((rhs.value/lhs.value), rhs.prog);}
-    bool is_older_than( const progVal& rhs ) 
-    { return rhs.prog < prog; }
-    void set(const V& val, const int& pr)
-    {value = val; prog = pr;}
-    
-  };
 
- 
+template<class V> struct progVal {
 
-  struct datasource {
-    miString name;
-    miString tag;
-  };
-  
-  
-  typedef set<miTime> timeline;
+  V value;
+  int prog;
 
-  template<class T> class tsData {
-  private:
-    //  parameter p;
-    datasource src;
-    map<miTime,progVal<T> > data;
-  public: 
-    tsData<T>() {}
-    tsData<T>(const tsData<T>& tsIn ) 
-    { copyMembers(tsIn) ; }
-    tsData<T>(const int & p, const int& s) 
-      : pindex(p), sindex(s) {}
+  progVal() :
+    prog(0)
+  {
+  }
+  progVal(V val) :
+    value(val), prog(0)
+  {
+  }
+  progVal(V val, int pr) :
+    value(val), prog(pr)
+  {
+  }
+  friend progVal<V> operator+(const progVal<V>& lhs, const progVal<V>& rhs)
+  {
+    return progVal((rhs.value + lhs.value), rhs.prog);
+  }
+  friend progVal<V> operator-(const progVal<V>& lhs, const progVal<V>& rhs)
+  {
+    return progVal((rhs.value - lhs.value), rhs.prog);
+  }
+  friend progVal<V> operator*(const progVal<V>& lhs, const progVal<V>& rhs)
+  {
+    return progVal((rhs.value * lhs.value), rhs.prog);
+  }
+  friend progVal<V> operator/(const progVal<V>& lhs, const progVal<V>& rhs)
+  {
+    return progVal((rhs.value / lhs.value), rhs.prog);
+  }
+  bool is_older_than(const progVal& rhs)
+  {
+    return rhs.prog < prog;
+  }
+  void set(const V& val, const int& pr)
+  {
+    value = val;
+    prog = pr;
+  }
 
-    // standart stuff. Get the data in and out again...
-  
-    int pindex,sindex;
-    bool empty() {return data.empty(); }
-    void  clean(){data.clear();}
-    void  push(const miTime t, const T val, const int& pr= 0)
-    { data[t] = progVal<T>(val,pr); }
-    
-    void setTs( map<miTime,progVal<T> > ts )
-    { data = ts ;}
-    
+};
 
-    T  value(miTime tim) {return data[tim].value;}
-    int prog(miTime tim) {return data[tim].prog;} 
-    map<miTime,progVal<T> > series() const    { return data;}
-    timeline times()    { return times(data);}
-    
-    tsData<T>& operator=(const tsData<T>& rhs )
-    { copyMembers(rhs); return *this; }
-    
-    progVal<T> operator[]( miTime req ) 
-    {
-      progVal<T> a;
-      return ( ( data.count(req) > 0 ) ? data[req] : a); 
-    }
-    
-    void copyMembers( const tsData<T>& rhs ){
-      src  = rhs.src;
-      data = rhs.data;
-      pindex = rhs.pindex;
-      sindex = rhs.sindex;
-      
-      // p = rhs.p;
-    }
-    // just for the test fun:
-  
-    friend ostream& operator<<(ostream& out, const tsData<T>& tsd){
-      map<miTime,progVal<T> > ts = tsd.series();
-    
-      typedef typename   map<miTime,progVal<T> >::iterator osTProgvalItr;
+struct datasource {
+  miutil::miString name;
+  miutil::miString tag;
+};
 
-      osTProgvalItr itr = ts.begin();
-      cout<<"valid time           prog   value"<<endl;
-      cout<<"---------------------------------"<<endl;
-      for ( ; itr != ts.end(); itr++)
-	out<<itr->first<<"  "
-	   <<itr->second.prog
-	   <<(itr->second.prog >= 100 ? "    " :"     ")
-	   <<itr->second.value
-	   <<endl;
-      return out;
-    }
-    
-  };
+typedef set<miutil::miTime> timeline;
 
+template<class T> class tsData {
+private:
+  //  parameter p;
+  datasource src;
+  map<miutil::miTime, progVal<T> > data;
+public:
+  tsData<T> ()
+  {
+  }
+  tsData<T> (const tsData<T>& tsIn)
+  {
+    copyMembers(tsIn);
+  }
+  tsData<T> (const int & p, const int& s) :
+    pindex(p), sindex(s)
+  {
+  }
+
+  // standart stuff. Get the data in and out again...
+
+  int pindex, sindex;
+  bool empty()
+  {
+    return data.empty();
+  }
+  void clean()
+  {
+    data.clear();
+  }
+  void push(const miutil::miTime t, const T val, const int& pr = 0)
+  {
+    data[t] = progVal<T> (val, pr);
+  }
+
+  void setTs(map<miutil::miTime, progVal<T> > ts)
+  {
+    data = ts;
+  }
+
+  T value(miutil::miTime tim)
+  {
+    return data[tim].value;
+  }
+  int prog(miutil::miTime tim)
+  {
+    return data[tim].prog;
+  }
+  map<miutil::miTime, progVal<T> > series() const
+  {
+    return data;
+  }
+  timeline times()
+  {
+    return times(data);
+  }
+
+  tsData<T>& operator=(const tsData<T>& rhs)
+  {
+    copyMembers(rhs);
+    return *this;
+  }
+
+  progVal<T> operator[](miutil::miTime req)
+  {
+    progVal<T> a;
+    return ((data.count(req) > 0) ? data[req] : a);
+  }
+
+  void copyMembers(const tsData<T>& rhs)
+  {
+    src = rhs.src;
+    data = rhs.data;
+    pindex = rhs.pindex;
+    sindex = rhs.sindex;
+
+    // p = rhs.p;
+  }
+  // just for the test fun:
+
+  friend ostream& operator<<(ostream& out, const tsData<T>& tsd)
+  {
+    map<miutil::miTime, progVal<T> > ts = tsd.series();
+
+    typedef typename map<miutil::miTime,progVal<T> >::iterator osTProgvalItr;
+
+    osTProgvalItr itr = ts.begin();
+    cout<<"valid time           prog   value"<<endl;
+    cout<<"---------------------------------"<<endl;
+    for (; itr != ts.end(); itr++)
+    out<<itr->first<<"  "
+    <<itr->second.prog
+    <<(itr->second.prog >= 100 ? "    " :"     ")
+    <<itr->second.value
+    <<endl;
+    return out;
+  }
+
+};
 
 };
 
 #endif
-
-
-
 
--- tsFunctions.h
+++ tsFunctions.h
@@ -1,32 +1,31 @@
 /*
-  libpuDatatypes - Diverse datatypes: Regions, coordinates and alike
-  
-  $Id$
+ libpuDatatypes - Diverse datatypes: Regions, coordinates and alike
 
-  Copyright (C) 2006 met.no
+ $Id$
 
-  Contact information:
-  Norwegian Meteorological Institute
-  Box 43 Blindern
-  0313 OSLO
-  NORWAY
-  email: diana@met.no
-  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ Copyright (C) 2006 met.no
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ Contact information:
+ Norwegian Meteorological Institute
+ Box 43 Blindern
+ 0313 OSLO
+ NORWAY
+ email: diana@met.no
 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #ifndef _tsFunctions_h
 #define _tsFunctions_h
@@ -34,7 +33,6 @@
 #include "tsData.h"
 
 #include <puTools/miTime.h>
-#include <puTools/miString.h>
 
 #include <map>
 #include <set>
@@ -42,126 +40,117 @@
 #include <functional>
 #include <algorithm>
 
-
 using namespace std;
 
 namespace puData {
 
-  // dance the timeseries
-  enum maskType {
-    AandB,
-    AorB,
-    AnotB
-  };
+// dance the timeseries
+enum maskType {
+  AandB, AorB, AnotB
+};
 
+template<class T> struct A_plus_B {
+  progVal<T> operator()(const progVal<T>& A, const progVal<T>& B)
+  {
+    return A + B;
+  }
+};
+template<class T> struct A_minus_B {
+  progVal<T> operator()(const progVal<T>& A, const progVal<T>& B)
+  {
+    return A - B;
+  }
+};
+template<class T> struct A_B_middle {
+  progVal<T> operator()(const progVal<T>& A, const progVal<T>& B)
+  {
+    return (A + B) / 2;
+  }
+};
+template<class T> struct prefer_A {
+  progVal<T> operator()(const progVal<T>& A, const progVal<T>& B)
+  {
+    return A;
+  }
+};
 
- 
-  template<class T> struct  A_plus_B {
-    progVal<T> operator()( const progVal<T>& A, const progVal<T>& B)
-    { return A+B;}
-  };	
-  template<class T> struct  A_minus_B {
-    progVal<T> operator()( const progVal<T>& A, const progVal<T>& B)
-    { return A-B;}
-  };
-  template<class T> struct  A_B_middle {
-    progVal<T> operator()( const progVal<T>& A, const progVal<T>& B)
-    { return (A+B)/2;}
-  };
-  template<class T> struct  prefer_A {
-    progVal<T> operator()( const progVal<T>& A, const progVal<T>& B)
-    { return A;}
-  };			
-  
-  
-  // subscribe one function object (functor): F 
-  // prefer_A<T> preferA; -> send in preferA
-  // define them yourself if you want to ... just one () operator
-  // and we're talking
-  // return value has to be progVal<T>
+// subscribe one function object (functor): F
+// prefer_A<T> preferA; -> send in preferA
+// define them yourself if you want to ... just one () operator
+// and we're talking
+// return value has to be progVal<T>
 
-  template<class T, typename F>
-  map<miTime,progVal<T> > mask(  map<miTime,progVal<T> > A,
-				 map<miTime,progVal<T> > B,
-				 F& fun,
-				 const maskType & mt = AandB){
-    map<miTime,progVal<T> > iSect;
-    map<miTime,progVal<T> > AB;
+template<class T, typename F>
+map<miutil::miTime, progVal<T> > mask(map<miutil::miTime, progVal<T> > A, map<
+    miutil::miTime, progVal<T> > B, F& fun, const maskType & mt = AandB)
+{
+  map<miutil::miTime, progVal<T> > iSect;
+  map<miutil::miTime, progVal<T> > AB;
 
-    typedef typename   map<miTime,progVal<T> >::iterator tPItr0;
+typedef  typename map<miutil::miTime,progVal<T> >::iterator tPItr0;
 
-    tPItr0 itr = A.begin();
-    
-    
+  tPItr0 itr = A.begin();
 
-    if(mt == AnotB ){
-      for( ; itr != A.end(); itr++)
-	if( !bool(B.count( itr->first )) ) 
-	  AB[itr->first] = itr->second;
-      return AB;
-    }
-    
-    for( ; itr != A.end(); itr++)
-      if( bool(B.count( itr->first ))) 
-	iSect[itr->first] = fun(itr->second, B[ itr->first ]);
-    
-    if(mt == AandB )
-      return iSect;
-    
-    AB = B;    
-    
-    for(itr = A.begin() ; itr != A.end(); itr++)
-      AB[itr->first] = ( bool(iSect.count( itr->first )) ? 
-			 iSect[itr->first ] : itr->second );    
+  if(mt == AnotB ) {
+    for(; itr != A.end(); itr++)
+    if( !bool(B.count( itr->first )) )
+    AB[itr->first] = itr->second;
     return AB;
-  };
+  }
 
-  // with no given function ( class F - > predef is not allowed !)
+  for(; itr != A.end(); itr++)
+  if( bool(B.count( itr->first )))
+  iSect[itr->first] = fun(itr->second, B[ itr->first ]);
 
-  template<class T>
-  map<miTime,progVal<T> > mask(  map<miTime,progVal<T> > A, 
-				 map<miTime,progVal<T> > B,
-				 const maskType & mt = AandB){
-    prefer_A<T> fun;
-    return mask (A,B,fun,mt);
-  };
+  if(mt == AandB )
+  return iSect;
 
+  AB = B;
 
-  // select a specific prog or discard one
-  template<class T>
-  map<miTime,progVal<T> > mask( const map<miTime,progVal<T> >& data,
-				const int& pr, const bool & incl = true  ){
+  for(itr = A.begin(); itr != A.end(); itr++)
+  AB[itr->first] = ( bool(iSect.count( itr->first )) ?
+      iSect[itr->first ] : itr->second );
+  return AB;
+};
 
-    
+// with no given function ( class F - > predef is not allowed !)
 
-    map<miTime,progVal<T> > ts = data;
-    typedef typename   map<miTime,progVal<T> >::iterator tPItr1;
-    tPItr1 itr = ts.begin();
-    for( ; itr != ts.end(); itr++)
-      if( (itr->second.prog == pr) == incl   ) 
-	ts.erase(itr);
-    return ts;
-  };
+template<class T>
+map<miutil::miTime,progVal<T> > mask( map<miutil::miTime,progVal<T> > A,
+    map<miutil::miTime,progVal<T> > B,
+    const maskType & mt = AandB) {
+  prefer_A<T> fun;
+  return mask (A,B,fun,mt);
+};
 
-  // getting the timeline
-  template<class T>
-  timeline times( const map<miTime,progVal<T> > & tsin ){
-    timeline t;
-    map<miTime,progVal<T> > ts = tsin;
+// select a specific prog or discard one
+template<class T>
+map<miutil::miTime,progVal<T> > mask( const map<miutil::miTime,progVal<T> >& data,
+    const int& pr, const bool & incl = true ) {
 
-    typedef typename   map<miTime,progVal<T> >::iterator tPItr2;
-    tPItr2 itr = ts.begin();
-    for(; itr != ts.end(); itr++)
-      t.insert( itr->first );
-    return t;
-  };
+  map<miutil::miTime,progVal<T> > ts = data;
+  typedef typename map<miutil::miTime,progVal<T> >::iterator tPItr1;
+  tPItr1 itr = ts.begin();
+  for(; itr != ts.end(); itr++)
+  if( (itr->second.prog == pr) == incl )
+  ts.erase(itr);
+  return ts;
+};
+
+// getting the timeline
+template<class T>
+timeline times( const map<miutil::miTime,progVal<T> > & tsin ) {
+  timeline t;
+  map<miutil::miTime,progVal<T> > ts = tsin;
+
+  typedef typename map<miutil::miTime,progVal<T> >::iterator tPItr2;
+  tPItr2 itr = ts.begin();
+  for(; itr != ts.end(); itr++)
+  t.insert( itr->first );
+  return t;
+};
 
 };
 
 #endif
-
-
-
-
-
 
